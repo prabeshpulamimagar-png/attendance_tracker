ef41f6503179bdc2a4d2624499aac76c012e6cd1
prabeshmagar <prabeshpulamimagar@gmail.com>
2026-09-09 20:37:27 +0400

    Add Flutter attendance tracker project files

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.attendance"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.attendance"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+flutter {
+    source = "../.."
+}
